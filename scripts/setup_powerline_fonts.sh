a0c7c4051577f6d3aababf1c6258c036418ba579
Wes Turner <wes@wrd.nu>
2023-07-20 21:52:27 -0400

    REF: scripts/setup_powerline_fonts.sh: only git clone if there's not a powerline-fonts pkg

--- scripts/setup_powerline_fonts.sh
+++ scripts/setup_powerline_fonts.sh
@@ -12,28 +12,29 @@ setup_powerline_fonts() {
         MACOS=true
     fi
 
-    if [ ! -d "powerline-fonts" ]; then
-        git clone https://github.com/powerline/fonts powerline-fonts
-    fi
-    cd powerline-fonts
-    if [ -n "${MACOS}" ]; then
-        find . -name '*.ttf' -exec cp "{}" ~/Library/Fonts \;
-        find . -name '*.otf' -exec cp "{}" ~/Library/Fonts \;
-        open "/Applications/Font Book.app"
 
-    elif [ -n "${APTGET}" ]; then
+    if [ -n "${APTGET}" ]; then
         sudo apt-get install -y fonts-powerline
 
     elif [ -n "${DNF}" ]; then
         sudo dnf install -y powerline-fonts
-
     else
-        mkdir -p ~/.fonts
-        find . -name '*.ttf' -exec cp "{}" ~/.fonts \;
-        find . -name '*.otf' -exec cp "{}" ~/.fonts \;
-        command -v fc-cache || \
-            echo 'fc-cache is not on $PATH; sudo dnf install -y fontconfig'
-        fc-cache -vf ~/.fonts
+        if [ ! -d "powerline-fonts" ]; then
+            git clone https://github.com/powerline/fonts powerline-fonts
+        fi
+        cd powerline-fonts
+        if [ -n "${MACOS}" ]; then
+            find . -name '*.ttf' -exec cp "{}" ~/Library/Fonts \;
+            find . -name '*.otf' -exec cp "{}" ~/Library/Fonts \;
+            open "/Applications/Font Book.app"
+        else
+            mkdir -p ~/.fonts
+            find . -name '*.ttf' -exec cp "{}" ~/.fonts \;
+            find . -name '*.otf' -exec cp "{}" ~/.fonts \;
+            command -v fc-cache || \
+                echo 'fc-cache is not on $PATH; sudo dnf install -y fontconfig'
+            fc-cache -vf ~/.fonts
+        fi
     fi
 }
 
